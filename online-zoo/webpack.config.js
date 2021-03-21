63e131086b2502f6826636d05e10554023662c26
Shamal Maksim <max.280599@gmail.com>
2021-03-21 22:30:00 +0300

    feat: w-2.0: import assets / inline svg / svgo
    import assest without copy-webpack-plugin / inline svg by query '.svg?i' / svgo formatter for inline svg

--- online-zoo/webpack.config.js
+++ online-zoo/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack')
@@ -14,6 +14,15 @@ const pageSetup = {
         title: 'Blank Page'
     }
 };
+
+const filenameGenerator = (fallbackPrepend = '') => ({
+    filename: (pathData) => {
+        const context = pathData?.module?.context;
+        const prepend = context ?
+            `../../${context.match(/assets\/.*/).pop()}/` : fallbackPrepend;
+        return prepend + '[name][ext]';
+    }
+});
 
 module.exports = {
     entry: {
@@ -35,18 +44,13 @@ module.exports = {
         port: 8080,
     },
     plugins: [
-        new CopyWebpackPlugin({ 
-            patterns: [{ 
-                from: './source/assets', 
-                to: path.resolve(__dirname, './build/assets') 
-            }] 
-        }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, pageSetup.indexHTML),
             filename: 'index.html',
             vars: { ...pageSetup.htmlVariables },
         }),
+        new HtmlWebpackInlineSVGPlugin(),
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css",
@@ -56,22 +60,32 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\/assets\//g,
-                type: 'asset/inline',
-                generator: { 
-                    dataUrl: (_, {filename}) => '../..' + filename.match(/\/assets\/.*/g)
-                }
+                test: /\.svg$/,
+                type: 'asset',
+                generator: filenameGenerator('../../assets/icons/'),
+            },
+            {
+                resourceQuery: /inline/,
+                type: 'asset/resource',
+                generator: filenameGenerator(''),
+                loader: 'svgo-loader',
+                options: { plugins: [
+                    { name: 'removeXMLNS' },
+                ]},
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                type: 'asset/resource',
+                generator: filenameGenerator('../../assets/images/'),
+            },
+            {
+                test: /\.woff2?$/,
+                type: 'asset/resource',
+                generator: filenameGenerator('../../assets/fonts/'),
             },
             {
                 test: /\.(scss|css)$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 
-                    {
-                        loader: 'css-loader', 
-                        options: { url: false } 
-                    }, 
-                    'sass-loader',
-                ],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'resolve-url-loader', 'sass-loader'],
             },
         ]
     },
