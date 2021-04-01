ff905d7f860df6f64f0282855664febe0a196f06
Shamal Maksim <max.280599@gmail.com>
2021-04-01 23:02:38 +0300

    pets [pets in zoo] @desktop [cards-generator.js]

---
+++ online-zoo/pages/landing/js/cards-generator.js
@@ -0,0 +1,90 @@
+const config = {
+    objects: (pathTo = '') => ([
+        {
+            name: 'Pandas',
+            description: 'Shenshuping Gengda Panda Center',
+            url: pathTo + 'xtina-yu.jpg',
+            linkTo: '../pages/zoos/panda/',
+        },
+        {
+            name: 'Eagles',
+            description: 'Catalina Island',
+            url: pathTo + 'nathan-lemon.jpg',
+            linkTo: '../pages/zoos/eagle/',
+        },
+        {
+            name: 'Gorillas',
+            description: 'GRACE gorillas',
+            url: pathTo + 'simbi-yvan.jpg',
+            linkTo: '../pages/zoos/gorilla/',
+        },
+        {
+            name: 'Aligators',
+            description: 'St. Augustine\'s Alligator Farm Zoological Park',
+            url: pathTo + 'matthew-essman.jpg',
+            linkTo: '../pages/zoos/alligator/',
+        },
+        {
+            name: 'Elephants',
+            description: 'Tembe Elephant Park',
+            url: pathTo + 'marc-bombenon.jpg',
+            linkTo: null,
+        },
+        {
+            name: 'Lemurs',
+            description: 'Madagascar',
+            url: pathTo + 'victoria-bragg.jpg',
+            linkTo: null,
+        },
+    ])  
+    // 'bob-walke.jpg', 'luca-abad-lopez.jpg',
+};
+
+const getCardTemplate = (
+    { name, description, url, linkTo },
+    iconPath = null,
+    clippedRectId = null,
+    cardColor = null
+) => {
+    const iconVar = iconPath ? `--bg-icon: url('${iconPath}')` : ``;
+    const cardColorVar = cardColor ? '--color: ${cardColor}' : ``;
+    /* CSS vars: --url, [--bg-icon], [--color] */
+    return (
+        `<article class="pet-card" 
+            style="--url: url('${url}'); ${iconVar} ${cardColorVar}">
+            <a class="pet-card__link" href="${linkTo ? linkTo : ``}">
+                <svg width="238" height="237" viewBox="0 0 238 237" class="pet-card__mask" fill="none">
+                    ${clippedRectId ? `<use xlink:href="${clippedRectId}" fill="white"/>` : ``}
+                </svg>
+                <div class="pet-card__description">
+                    <h4 class="pet-card__heading">${name}</h4>
+                    <p class="pet-card__paragraph">${description}</p>
+                </div>
+            </a>
+        </article>`
+    );
+};
+
+const getNode = (htmlString) => {
+    const template = document.createElement('template');
+    template.innerHTML = htmlString.trim();
+    return template.content.firstChild;
+};
+
+const generateCards = (container, config) => {
+    const getCardTemplateConfigured = (cardConfig) => {
+        return getCardTemplate(
+            cardConfig,
+            iconPath = '../../../assets/icons/tv.svg',
+            clippedRectId = '#clippedRect',
+    )};
+    config.objects('../../../assets/images/pet-cards/')
+        .map(cardConfig => getCardTemplateConfigured(cardConfig))
+        .map(cardTemplate => getNode(cardTemplate))
+        .forEach(cardNode => {
+            container.appendChild(cardNode)
+        });
+};
+
+generateCards(
+    document.querySelector('.pets__cards-container'), config);
